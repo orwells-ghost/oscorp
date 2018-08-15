c47fc596c7d44cd30cc794149ef52d6541f3a206
Ryan Kennedy <ryan+github@ryankennedy.me>
2018-08-14 20:53:45 -0700

    First commit

---
+++ js/scripts.js
@@ -0,0 +1,61 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  var links = document.getElementById('navbar').getElementsByTagName('a');
+  var navLinks = document.getElementById('navLinks').getElementsByTagName('a');
+  
+  if (document.body.scrollTop > 25 || document.documentElement.scrollTop > 25) {
+    document.getElementById("navbar").style.padding = "1.7rem 6.4rem";
+    document.getElementById("navbar").style.backgroundColor = "#fff";
+    var k;
+    for (k = 0; k < links.length; k++) {
+        links[k].style.color = "#000";
+    }
+  } else {
+    document.getElementById("navbar").style.padding = "3.4rem 6.4rem";
+    document.getElementById("navbar").style.backgroundColor = "transparent";
+    var k;
+    for (k = 0; k < links.length; k++) {
+        links[k].style.color = "#fff";
+    }
+  }
+
+  if (document.documentElement.scrollTop < 550) {
+    navLinks[0].classList.add('active');
+    navLinks[1].classList.remove('active');
+    console.log("hit me!");
+  } else {
+    navLinks[0].classList.remove('active');
+  }
+}
+
+var header = document.getElementById("navbar");
+var btns = header.getElementsByClassName("header__nav-link");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+var sections = $('section')
+  , nav = $('nav')
+  , nav_height = nav.outerHeight();
+ 
+$(window).on('scroll', function () {
+  var cur_pos = $(this).scrollTop();
+ 
+  sections.each(function() {
+    var top = $(this).offset().top - nav_height,
+        bottom = top + $(this).outerHeight();
+ 
+    if (cur_pos >= top && cur_pos <= bottom) {
+      nav.find('a').removeClass('active');
+      sections.removeClass('active');
+ 
+      $(this).addClass('active');
+      nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+    }
+  });
+});
