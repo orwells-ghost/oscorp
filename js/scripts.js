028bcae9c828c89736f004995efafc768404b90f
Ryan Kennedy <ryan+github@ryankennedy.me>
2018-08-27 18:46:36 -0700

    Added scroll-to-top button and portfolio filter

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,23 @@
+// Detect scroll and change nav and scrollbox
 window.onscroll = function() {scrollFunction()};
+var navbar = document.getElementById("navbar");
+var links = document.getElementById('navbar').getElementsByTagName('a');
+var navUL = document.getElementById('navUL');
+var navLinks = navUL.getElementsByTagName('a');
+var logo = document.getElementById('logo');
+var scrollBox = document.getElementById("scrollBox");
 
 function scrollFunction() {
-  var links = document.getElementById('navbar').getElementsByTagName('a');
-  var navLinks = document.getElementById('navLinks').getElementsByTagName('a');
   
   if (document.body.scrollTop > 25 || document.documentElement.scrollTop > 25) {
-    document.getElementById("navbar").style.padding = "1.7rem 6.4rem";
-    document.getElementById("navbar").style.backgroundColor = "#fff";
-    document.getElementById('navLinks').style.top = "7.15rem";
+    // Scroll button
+    scrollBox.style.opacity = "1"; 
+    // setTimeout(function(){ scrollBox.style.opacity = "1"; }, 300);
+
+    // Nav
+    navbar.style.padding = "1.7rem 6.4rem";
+    navbar.style.backgroundColor = "#fff";
+    navUL.style.top = "7.15rem";
     if (screen.width >= 1000) {
       for (var i = 0; i < links.length; i++) {
         if (i === 0) {
@@ -17,20 +27,24 @@ function scrollFunction() {
         }
       }
     }
-    document.getElementById('logo').style.color = "#26b18a";
+    logo.style.color = "#26b18a";
   } else {
-    document.getElementById("navbar").style.padding = "3.4rem 6.4rem";
-    document.getElementById("navbar").style.backgroundColor = "transparent";
-    document.getElementById('navLinks').style.top = "10.5rem";
-    if (screen.width >= 1000) {
-      for (var i = 0; i < links.length; i++) {
-        if (i === 0) {
-          continue;
-        } else {
-          links[i].style.color = "#fff";
+      // Scroll box
+      scrollBox.style.opacity = "0";
+
+      // Nav
+      navbar.style.padding = "3.4rem 6.4rem";
+      navbar.style.backgroundColor = "transparent";
+      navUL.style.top = "10.5rem";
+      if (screen.width >= 1000) {
+        for (var i = 0; i < links.length; i++) {
+          if (i === 0) {
+            continue;
+          } else {
+            links[i].style.color = "#fff";
+          }
         }
       }
-    }
   }
 
   if (document.documentElement.scrollTop < 550) {
@@ -41,6 +55,7 @@ function scrollFunction() {
   }
 }
 
+// Change nav link active state on click
 var header = document.getElementById("navbar");
 var btns = header.getElementsByClassName("header__nav-link");
 for (var i = 0; i < btns.length; i++) {
@@ -51,6 +66,7 @@ for (var i = 0; i < btns.length; i++) {
   });
 }
 
+// Change nav link active state based on scroll position
 var sections = $('section')
   , nav = $('nav')
   , nav_height = nav.outerHeight();
@@ -69,9 +85,44 @@ $(window).on('scroll', function () {
   });
 });
 
+// When the user clicks scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
+
+// Own carousel
 $('.owl-carousel').owlCarousel({
   center: true,
   items: 1,
   center:true,
   dots: true,
 });
+
+// Portfolio Filter
+var portfolioItems = document.getElementsByClassName("portfolio__item");
+
+function filterItem(filter){
+  for (var i = 0; i < portfolioItems.length; i++){
+    if (portfolioItems[i].dataset.filter !== filter){
+      portfolioItems[i].classList.add("displayNone");
+    } else {
+      portfolioItems[i].classList.remove("displayNone");
+    }
+  }
+}
+
+function allFilter(){
+  for (var i = 0; i < portfolioItems.length; i++){
+    portfolioItems[i].classList.remove("displayNone");
+  }
+}
+
+var btns = document.getElementsByClassName("portfolio__button");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("active-filter");
+    current[0].className = current[0].className.replace(" active-filter", "");
+    this.className += " active-filter";
+  });
+}
